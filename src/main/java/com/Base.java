d6be149bf28009abc467f27dcc2004761f695c91
MilaRossi <liudmila.rossi@gmail.com>
2022-03-23 10:47:41 +0100

    added System.getProperty("baseUrl");

--- src/main/java/com/Base.java
+++ src/main/java/com/Base.java
@@ -5,7 +5,7 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.http.ContentType.JSON;
 
 public class Base {
-    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/api/";
+    public static final String BASE_URL = System.getProperty("baseUrl");
 
     public static RequestSpecification getBaseSpec() {
         return new RequestSpecBuilder()
